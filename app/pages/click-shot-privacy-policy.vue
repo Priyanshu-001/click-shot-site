1d237f979698d53efd6e7e009b9582bee552686d
priyanshu-001 <26710303+Priyanshu-001@users.noreply.github.com>
2026-01-25 23:12:49 +0530

    added: privacy policy

---
+++ app/pages/click-shot-privacy-policy.vue
@@ -0,0 +1,30 @@
+<template>
+ <div>
+      <UContainer class="relative py-24">
+    <h1 class="text-4xl md:text-5xl font-bold leading-tight">Privacy Policy</h1>
+    <h3 class="font-extra-thin italic"> Privacy policy for clickShot  </h3>
+         <ul class="list-disc list-inside space-y-2 my-4 md:mx-4">
+
+                <li>
+                    ClickShot does not collect, transmit, or share any personal data.
+                </li>
+
+                <li>
+                    The extension is used only to capture video frames from the current tab and
+                    allow users to annotate them with personal notes. All screenshots and notes
+                    are stored locally on the user’s device using browser storage.
+                </li>
+
+                <li>
+                    ClickShot does not send data to any external servers, does not use analytics,
+                    tracking, or advertising, and does not include any third-party services.
+                </li>
+
+                <li>
+                    All data remains under the user’s control and is permanently removed when the
+                    extension is uninstalled.
+                </li>
+            </ul>
+    </UContainer>
+ </div>    
+</template>
